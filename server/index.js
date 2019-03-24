0b0c357500d902bf6276f5fa36697dfe06fb01c7
chrismooreproductions <chrismooreproductions@gmail.com>
2019-03-24 17:57:28 +0000

    Sorted out the start up process for the production version of the app.
    Added cross-env lib for NODE_ENV value.
    
    In production the app is served from the `./dist` directory and the app runs from express on port 3001.
    
    New scripts added to package.json for the build processes (`build:app` and `build:server`) and for the `start` process.

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const app = express()
+const opn = require('opn');
+
+const port = process.env.PORT || 3001
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(__dirname +'./../dist'));
+}
 
 app.post('/api/submit-survey', (request, response) => {
   console.log(request.body)
   response.send({ message: "you hit the submit survey endpoint!" });
 })
 
-app.listen(3001, () =>
-  console.log('Express server is running on localhost:3001')
+app.listen(port, () =>
+  console.log(`Express server is running on localhost:${port}`)
 );
+
+if (process.env.NODE_ENV === "production") {
+  opn(`http://localhost:${port}`);
+}
